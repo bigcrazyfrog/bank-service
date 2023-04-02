4b2170ce8ad8ee7f0ecf24baa52f04fa6b37df02
Biven1 <cibinnikita@gmail.com>
2023-04-02 13:11:58 +0500

    bot webhook

--- src/app/internal/bot.py
+++ src/app/internal/bot.py
@@ -38,5 +38,5 @@ def bot_webhook():
         listen='127.0.0.1',
         port=8443,
         url_path='TOKEN',
-        webhook_url='https://nikita.backend23.2tapp.cc:8443/TOKEN',
+        webhook_url='http://127.0.0.1:8443/TOKEN/',
     )
