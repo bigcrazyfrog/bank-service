45519e60cec60fe295d49af39e567d428194dad0
Nikita <90467228+bigcrazyfrog@users.noreply.github.com>
2023-06-19 23:59:21 +0500

    replace admin_user

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,4 @@
 from app.internal.auth.db.models import RefreshToken
 from app.internal.bank.db.models import Account, Transaction
-from app.internal.users.db.models import AdminUser, User
+from app.internal.users.db.models import User
+from app.internal.admin_users.db.models import AdminUser
